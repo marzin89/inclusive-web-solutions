2e22b33ae964919fd3e99beba8e8bd97c6c43cee
marco <mazi2001@student.miun.se>
2022-11-23 22:15:19 +0100

    Replace localStorage with the language selector from the store

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,10 @@
-// Imports
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 // Sidfot
 function Footer() {
+    const language = useSelector((state) => state.page.language);
+
     return (
         <footer>
             <div id="footer-wrapper">
@@ -12,7 +14,7 @@ function Footer() {
                         regular-font-size" href="mailto:info@iws.se">
                             info@iws.se</a> | 0123-45678 | Drottninggatan 1, 12345 Grönköping</p>
                     {/* Länk till integritetspolicy */}
-                    {localStorage.getItem('language') == 'Deutsch' ? 
+                    {language == 'Deutsch' ? 
                     <p id="privacy-policy"><a id="privacy-policy-link" href="" 
                         className="focus focus-invisible regular-font-size">Datenschutzerklärung</a></p> 
                     :
@@ -24,7 +26,7 @@ function Footer() {
                 </div>
                 {/* Tillbaka-till-toppen-knapp */}
                 <a id="scroll-to-top-btn" className="focus focus-invisible" role="button" href="#main"
-                    aria-label={localStorage.getItem('language') == 'Deutsch' ? 'Zum Seitenanfang springen' : 
+                    aria-label={language == 'Deutsch' ? 'Zum Seitenanfang springen' : 
                     'Gå till toppen av sidan'}>
                     <svg role="button" width="45" height="25">
                         <line x1="2" x2="22" y1="22" y2="2" stroke="white" strokeWidth="5"></line>
