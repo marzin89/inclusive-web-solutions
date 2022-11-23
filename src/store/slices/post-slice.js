48b6707fda4fd6ad340ccc6e5a14b2bf160198aa
marco <mazi2001@student.miun.se>
2022-11-23 23:46:42 +0100

    Add a post slice

---
+++ src/store/slices/post-slice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const postSlice = createSlice({
+    name: 'post',
+    initialState: {
+        swedish: [],
+        german: [],
+        posts: [],
+        post: [],
+    },
+    reducers: {
+        setPosts(state = initialState, action) {
+            let swedish = [];
+            let german = [];
+
+            action.payload.map((post) => {
+                if (post.language == 'swedish') {
+                    swedish.push(post);
+                
+                } else {
+                    german.push(post);
+                }}
+            );
+
+            return {
+                ...state,
+                swedish: swedish,
+                german: german,
+            };
+        },
+    }
+});
+
+export const postActions = postSlice.actions;
+export default postSlice;
