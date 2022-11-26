f36eba9f1e59e5923c4ce78d5d028ac440d21b6d
marco <mazi2001@student.miun.se>
2022-11-26 21:18:06 +0100

    Import the solution and course slices

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,19 @@ import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import pageSlice from './slices/page-slice';
 import testSlice from './slices/test-slice';
+import solutionSlice from './slices/solution-slice';
+import courseSlice from './slices/course-slice';
 import userSlice from './slices/user-slice';
 import postSlice from './slices/post-slice';
 
 const rootReducer = combineReducers({
     page: pageSlice.reducer,
     test: testSlice.reducer,
+    solution: solutionSlice.reducer,
+    course: courseSlice.reducer,
     post: postSlice.reducer,
     user: userSlice.reducer,
     /*
-    solution: solutionSlice.reducer,
-    course: courseSlice.reducer,
     comment: commentSlice.reducer,
     search: searchSlice.reducer,
     */
