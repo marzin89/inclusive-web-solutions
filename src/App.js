d4d7d777e21bb9c1911134828c6bffa0ecb6afa2
marco <mazi2001@student.miun.se>
2022-11-27 14:37:19 +0100

    Fetch all solutions and dispatch to the store

--- src/App.js
+++ src/App.js
@@ -91,6 +91,34 @@ function App() {
     });
   }
 
+  function getSolutions() {
+    fetch('https://iws-rest-api.herokuapp.com/solutions')
+    .then(response => response.json())
+    .then(data => {
+      if (!data.length) {
+        if (language == 'Swedish') {
+          dispatch(solutionActions.setErrorMessage('Inga utvecklingspaket hittades.'));
+
+        } else {
+          dispatch(solutionActions.setErrorMessage('Es wurden keine Development-pakete gefunden.'));
+        }
+
+      } else {
+        dispatch(solutionActions.setSolutions(data));
+      }
+    })
+    .catch(() => {
+      if (language == 'Swedish') {
+        dispatch(solutionActions.setErrorMessage('Ett serverfel har uppstått.' 
+          + 'Det gick inte att hämta utvecklingspaket. Försök igen lite senare.'));
+      
+      } else {
+        dispatch(solutionActions.setErrorMessage('Ein Serverfehler ist aufgetreten.' 
+          + 'Es konnten keine Development-Pakete abgerufen werden. Versuchen Sie es später erneut.'));
+      }
+    });
+  }
+
   useEffect(() => {
     getPosts();
     getTests();
