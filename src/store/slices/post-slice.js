667e06e35d6f817610f00dbdd4c413c84a604d2e
marco <mazi2001@student.miun.se>
2022-11-27 14:22:53 +0100

    Filter and store the three most recent posts in each language

--- src/store/slices/post-slice.js
+++ src/store/slices/post-slice.js
@@ -3,6 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 const postSlice = createSlice({
     name: 'post',
     initialState: {
+        featuredSwedish: [],
+        featuredGerman: [],
         swedish: [],
         german: [],
         posts: [],
@@ -13,18 +15,32 @@ const postSlice = createSlice({
         setPosts(state, action) {
             let swedish = [];
             let german = [];
+            let featuredSwedish = [];
+            let featuredGerman = [];
 
-            action.payload.map((post) => {
+            action.payload.map((post, index) => {
                 if (post.language == 'swedish') {
                     swedish.push(post);
                 
                 } else {
                     german.push(post);
-                }}
+                }
+                
+                if (index < 3) {
+                    if (post.language == 'swedish') {
+                        featuredSwedish.push(post);
+                    
+                    } else {
+                        featuredGerman.push(post);
+                    }
+                }
+            }
             );
 
             return {
                 ...state,
+                featuredSwedish: featuredSwedish,
+                featuredGerman: featuredGerman,
                 swedish: swedish,
                 german: german,
             };
