a0dee47a0e9f9022ee13944ee87d62ec0123322b
marco <mazi2001@student.miun.se>
2022-12-12 21:50:11 +0100

    Filter, set and store the search results, number of pages, the active page, the search index and an error message in Swedish and German

--- src/store/slices/page-slice.js
+++ src/store/slices/page-slice.js
@@ -7,6 +7,17 @@ const pageSlice = createSlice({
         query: '',
         accessibility: 'standard',
         previous: '',
+        searchResultsSwedish: [],
+        searchResultsGerman: [],
+        pageSwedish: [],
+        pageGerman: [],
+        numberOfPagesSwedish: 0,
+        numberOfPagesGerman: 0,
+        activeSearchPageSwedish: 0,
+        activeSearchPageGerman: 0,
+        searchIndexSwedish: 0,
+        searchIndexGerman: 0,
+        errorMessage: '',
     },
     reducers: {
         setLanguage(state, action) {
@@ -26,6 +37,101 @@ const pageSlice = createSlice({
                 ...state,
                 previous: state.accessibility,
                 accessibility: action.payload,
+            };
+        },
+        search(state, action) {
+            let resultsSwedish = [];
+            let resultsGerman = [];
+            let numberOfPagesSwedish;
+            let numberOfPagesGerman;
+
+            action.payload.foreach((page) => {
+                if (page.title.toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
+                    page.content.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+
+                    if (language == 'Swedish') {
+                        resultsSwedish.push(page);
+                    
+                    } else {
+                        resultsGerman.push(page);
+                    }
+                }
+            });
+
+            if (resultsSwedish.length > 5) {
+                numberOfPagesSwedish = Math.ceil(resultsSwedish.length / 5);
+            
+            } else {
+                numberOfPagesSwedish = 1;
+            }
+
+            if (resultsGerman.length > 5) {
+                numberOfPagesGerman = Math.ceil(resultsGerman.length / 5);
+            
+            } else {
+                numberOfPagesGerman = 1;
+            }
+
+            return {
+                ...state,
+                searchResultsSwedish: resultsSwedish,
+                searchResultsGerman: resultsGerman,
+                numberOfPagesSwedish: numberOfPagesSwedish,
+                numberOfPagesGerman: numberOfPagesGerman,
+            };
+        },
+        toggleSwedish(state, action) {
+            let index;
+            let page = [];
+
+            if (action.payload == 1) {
+                index = 0;
+            
+            } else {
+                index = (action.payload - 1) * 5;
+            }
+
+            for (let i = index; i < (index + 5); i++) {
+                if (state.searchResultsSwedish[i]) {
+                    page.push(state.searchResultsSwedish[i]);
+                }
+            }
+
+            return {
+                ...state,
+                pageSwedish: page,
+                activeSearchPageSwedish: action.payload,
+                searchIndexSwedish: index,
+            };
+        },
+        toggleGerman(state, action) {
+            let index;
+            let page = [];
+
+            if (action.payload == 1) {
+                index = 0;
+            
+            } else {
+                index = (action.payload - 1) * 5;
+            }
+
+            for (let i = index; i < (index + 5); i++) {
+                if (state.searchResultsGerman[i]) {
+                    page.push(state.searchResultsGerman[i]);
+                }
+            }
+
+            return {
+                ...state,
+                pageGerman: page,
+                activeSearchPageGerman: action.payload,
+                searchIndexGerman: index,
+            };
+        },
+        setErrorMessage(state, action) {
+            return {
+                ...state,
+                errorMessage: action.payload,
             };
         },
     }
